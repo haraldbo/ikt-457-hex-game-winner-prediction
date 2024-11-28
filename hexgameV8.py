0470d97a8512108d2357d652502c5eed4b0e9eb2
Harald Borgenvik <8281021+haraldbo@users.noreply.github.com>
2024-11-28 22:10:13 +0100

    Update hexgameV8.py

--- hexgameV8.py
+++ hexgameV8.py
@@ -11,6 +11,7 @@ from utils import load_dataset, create_graph, get_neighbour_lookup, get_all_boar
 from networkx import has_path
 from tqdm import tqdm
 import networkx as nx
+from sklearn.model_selection import train_test_split
 
 RED = -1
 EMPTY = 0
@@ -20,17 +21,17 @@ lookups = {}
 
 def default_args(**kwargs):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", default=3000, type=int)
-    parser.add_argument("--number-of-clauses", default=20000, type=int)
+    parser.add_argument("--epochs", default=50, type=int)
+    parser.add_argument("--number-of-clauses", default=5000, type=int)
     parser.add_argument("--T", default=20000, type=int)
-    parser.add_argument("--s", default=15.0, type=float)
-    parser.add_argument("--depth", default=2, type=int)
-    parser.add_argument("--hypervector-size", default=1024, type=int)
-    parser.add_argument("--hypervector-bits", default=2, type=int)
+    parser.add_argument("--s", default=17.0, type=float)
+    parser.add_argument("--depth", default=1, type=int)
+    parser.add_argument("--hypervector-size", default=512, type=int)
+    parser.add_argument("--hypervector-bits", default=1, type=int)
     parser.add_argument("--message-size", default=256, type=int)
-    parser.add_argument("--message-bits", default=2, type=int)
+    parser.add_argument("--message-bits", default=1, type=int)
     parser.add_argument('--double-hashing', dest='double_hashing', default=False, action='store_true')
-    parser.add_argument("--max-included-literals", default=500, type=int)
+    parser.add_argument("--max-included-literals", default=None, type=int)
     parser.add_argument("--number-of-state-bits", default=16, type=int)
 
     args = parser.parse_args()
@@ -117,106 +118,117 @@ def populate_graphs(X: np.ndarray, graphs: Graphs, board_size):
                     
     graphs.encode()
     return graphs
+
+def write_tm_outputs_to_file(tm: MultiClassGraphTsetlinMachine, args, graphs_test):
+    file = open("output.txt", "w+")
+
+    def fprint(line, end = "\n"):
+        file.write(f"{line}{end}")
+
+    weights = tm.get_state()[1].reshape(2, -1)
+    for i in range(tm.number_of_clauses):
+            fprint("Clause #%d W:(%d %d)" % (i, weights[0,i], weights[1,i]), end=' ')
+            l = []
+            for k in range(args.hypervector_size * 2):
+                if tm.ta_action(0, i, k):
+                    if k < args.hypervector_size:
+                        l.append("x%d" % (k))
+                    else:
+                        l.append("NOT x%d" % (k - args.hypervector_size))
+
+            # for k in range(args.message_size * 2):
+            #     if tm.ta_action(1, i, k):
+            #         if k < args.message_size:
+            #             l.append("c%d" % (k))
+            #         else:
+            #             l.append("NOT c%d" % (k - args.message_size))
+
+            fprint(" AND ".join(l))
+
+
+    #fprint(graphs_test.hypervectors)
+    fprint(tm.hypervectors)
+    #fprint(graphs_test.edge_type_id)
+
+    file.close()
     
-args = default_args()
+def train():
+    args = default_args()
+    # Create train data
 
-# Create train data
+    num_rows = 1000
+    board_size = 9
+    X, Y = load_dataset("hex_9x9_2moves.csv", num_rows = num_rows)
+    print("Possible connections: ", len(get_all_possible_connections(board_size)))
+    print("Total number of symbols: ", len(get_all_symbols(board_size)))
 
-num_rows = None
-board_size = 9
-X, Y = load_dataset("hex_9x9_5moves.csv", num_rows = num_rows)
-print("Possible connections: ", len(get_all_possible_connections(board_size)))
-print("Total number of symbols: ", len(get_all_symbols(board_size)))
+    Y = np.where(Y > 0, 1, 0)
 
-Y = np.where(Y > 0, 1, 0)
+    # First 80% of data is training, the remaining is test
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify=Y, test_size=0.2)
 
-# First 80% of data is training, the remaining is test
-split_index = int(0.8 * len(X))
-X_train = X[:split_index]
-Y_train = Y[:split_index]
-X_test = X[split_index:]
-Y_test = Y[split_index:]
+    print("Train balance:")
+    unique, counts = np.unique(Y_train, return_counts=True)
+    print(np.asarray((unique, counts)).T)
+    print(counts/counts.sum())
 
-print("Train balance:")
-unique, counts = np.unique(Y_train, return_counts=True)
-print(np.asarray((unique, counts)).T)
-print(counts/counts.sum())
+    print("Test balance:")
+    unique, counts = np.unique(Y_test, return_counts=True)
+    print(np.asarray((unique, counts)).T)
+    print(counts/counts.sum())
 
-print("Test balance:")
-unique, counts = np.unique(Y_test, return_counts=True)
-print(np.asarray((unique, counts)).T)
-print(counts/counts.sum())
+    print("Creating training graphs.")
+    graphs_train = Graphs(
+        X_train.shape[0],
+        symbols=get_all_symbols(board_size),
+        hypervector_size=args.hypervector_size,
+        hypervector_bits=args.hypervector_bits,
+    )
+    graphs_train = populate_graphs(X_train, graphs_train, board_size)
+    print("Done.")
 
-print("Creating training graphs.")
-graphs_train = Graphs(
-    X_train.shape[0],
-    symbols=get_all_symbols(board_size),
-    hypervector_size=args.hypervector_size,
-    hypervector_bits=args.hypervector_bits,
-)
-graphs_train = populate_graphs(X_train, graphs_train, board_size)
-print("Done.")
+    print("Creating test graphs.")
+    graphs_test = Graphs(X_test.shape[0], init_with=graphs_train)
+    graphs_test = populate_graphs(X_test, graphs_test, board_size)
+    print("Done.")
 
-print("Creating test graphs.")
-graphs_test = Graphs(X_test.shape[0], init_with=graphs_train)
-graphs_test = populate_graphs(X_test, graphs_test, board_size)
-print("Done.")
+    print("Initializing tsetlin machine.")
+    tm = MultiClassGraphTsetlinMachine(
+        args.number_of_clauses,
+        args.T,
+        args.s,
+        number_of_state_bits = args.number_of_state_bits,
+        depth = args.depth,
+        message_size = args.message_size,
+        message_bits = args.message_bits,
+        max_included_literals = args.max_included_literals,
+        double_hashing = args.double_hashing,
+        grid=(16*13,1,1),
+        block=(128,1,1)
+    )
 
-print("Initializing tsetlin machine.")
-tm = MultiClassGraphTsetlinMachine(
-    args.number_of_clauses,
-    args.T,
-    args.s,
-    number_of_state_bits = args.number_of_state_bits,
-    depth = args.depth,
-    message_size = args.message_size,
-    message_bits = args.message_bits,
-    max_included_literals = args.max_included_literals,
-    double_hashing = args.double_hashing,
-    grid=(16*13,1,1),
-    block=(128,1,1)
-)
+    print("Starting training..")
+    TS = time.strftime("%Y%m%d_%H%M%S")
+    stats_file = f"train_{TS}.csv"
+    print("Appending statistics to", stats_file)
 
-print("Starting training..")
-TS = time.strftime("%Y%m%d_%H%M%S")
-stats_file = f"train_{TS}.csv"
-print("Appending statistics to", stats_file)
-append_to_statistics_file(stats_file, "train accuracy", "test accuracy")
-for i in range(args.epochs):
-    start_training = time.time()
-    tm.fit(graphs_train, Y_train, epochs=1, incremental=True)
-    stop_training = time.time()
+    append_to_statistics_file(stats_file, "train accuracy", "test accuracy")
+    for i in range(args.epochs):
+        start_training = time.time()
+        tm.fit(graphs_train, Y_train, epochs=1, incremental=True)
+        stop_training = time.time()
 
-    start_testing = time.time()
-    result_test = 100*(tm.predict(graphs_test) == Y_test).mean()
-    stop_testing = time.time()
+        start_testing = time.time()
+        result_test = 100*(tm.predict(graphs_test) == Y_test).mean()
+        stop_testing = time.time()
+        
+        result_train = 100*(tm.predict(graphs_train) == Y_train).mean()
+        
+        append_to_statistics_file(stats_file, str(result_train), str(result_test))
+        print("%d %.2f %.2f %.2f %.2f" % (i, result_train, result_test, stop_training-start_training, stop_testing-start_testing))
 
-    result_train = 100*(tm.predict(graphs_train) == Y_train).mean()
-    
-    append_to_statistics_file(stats_file, str(result_train), str(result_test))
+    write_tm_outputs_to_file(tm, args, graphs_test)
 
-    print("%d %.2f %.2f %.2f %.2f" % (i, result_train, result_test, stop_training-start_training, stop_testing-start_testing))
 
-weights = tm.get_state()[1].reshape(2, -1)
-for i in range(tm.number_of_clauses):
-        print("Clause #%d W:(%d %d)" % (i, weights[0,i], weights[1,i]), end=' ')
-        l = []
-        for k in range(args.hypervector_size * 2):
-            if tm.ta_action(0, i, k):
-                if k < args.hypervector_size:
-                    l.append("x%d" % (k))
-                else:
-                    l.append("NOT x%d" % (k - args.hypervector_size))
-
-        # for k in range(args.message_size * 2):
-        #     if tm.ta_action(1, i, k):
-        #         if k < args.message_size:
-        #             l.append("c%d" % (k))
-        #         else:
-        #             l.append("NOT c%d" % (k - args.message_size))
-
-        print(" AND ".join(l))
-
-print(graphs_test.hypervectors)
-print(tm.hypervectors)
-print(graphs_test.edge_type_id)
+if __name__ == '__main__':  
+    train()
